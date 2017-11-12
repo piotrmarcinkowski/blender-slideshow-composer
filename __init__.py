596309380fe9099a0d76406171156797acf53ca4
User <User>
2017-11-12 22:35:11 +0100

    Add initial files

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "SlideShow Composer",
+    "description": "Helps creating slide shows from pictures/videos",
+    "author": "Piotr Marcinkowski",
+    "version": (0, 1, 0),
+    "blender": (2, 78, 0),
+    "location": "sequencer",    
+    "wiki_url": "",    
+    "category": "VSE"
+}
+
+import bpy
+import os
+
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
