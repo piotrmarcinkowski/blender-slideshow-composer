57725990566ca1d8b442a4818cb78b5b71e7ea5f
Piotr Marcinkowski <piotrmarc@gmail.com>
2018-01-13 21:33:15 +0100

    Add creating image strips of imported files

--- operators.py
+++ operators.py
@@ -13,17 +13,38 @@ class ImportFiles(bpy.types.Operator):
         type=bpy.types.OperatorFileListElement,
     )
 
+    image_strip_frames = bpy.props.IntProperty(
+        name="Image strip frames",
+        description="How long imported images are displayed during playback",
+        default=120,
+        min=30)
+
     @classmethod
     def poll(self, context):
         return True
 
     def execute(self, context):
-        print("execute Import files from '{}'".format(self.filepath))
+        self.create_strips()
         return {'FINISHED'}
 
     def invoke(self, context, event):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
+
+    def create_strips(self):
+        """ Creates strips for imported files """
+        frame_current = bpy.context.scene.frame_current
+
+        for file in self.files:
+            file_list = [{'name': file['name']}]
+            bpy.ops.sequencer.image_strip_add(
+                directory=self.directory,
+                files=file_list,
+                show_multiview=False,
+                frame_start=frame_current,
+                frame_end=frame_current + self.image_strip_frames,
+                channel=1)
+            frame_current += self.image_strip_frames
 
 #if __name__ == "__main__":
 #    bpy.utils.register_class(ImportFiles)
