acbed3853b081ef1046213f373f72e771518fb96
Piotr Marcinkowski <piotrmarc@gmail.com>
2018-01-24 23:31:06 +0100

    Add working Ken Burns effect - scaling only for now

--- operator_import.py
+++ operator_import.py
@@ -43,6 +43,7 @@ class ImportFiles(bpy.types.Operator):
         """ Creates strips for imported files """
         frame_current = bpy.context.scene.frame_current
         previous_strip = None
+        previous_transform_strip = None
 
         for file in self.files:
             frame_start = frame_current
@@ -53,17 +54,20 @@ class ImportFiles(bpy.types.Operator):
                 frame_end=frame_end,
                 channel=1)
 
-            self.create_transform(
+            current_transform_strip = self.create_transform_strip(
                 frame_start=frame_start,
                 frame_end=frame_end)
 
-            if previous_strip is not None:
+            current_strip.mute = True
+
+            if previous_transform_strip is not None:
                 self.create_cross(
-                    first_strip=previous_strip,
-                    second_strip=current_strip)
+                    first_strip=previous_transform_strip,
+                    second_strip=current_transform_strip)
 
             frame_current = frame_end + 1 - self.strips_cross_frames
             previous_strip = current_strip
+            previous_transform_strip = current_transform_strip
 
     def create_strip(self, path, frame_start, frame_end, channel):
         file_list = [{'name': path}]
@@ -78,25 +82,20 @@ class ImportFiles(bpy.types.Operator):
 
         return bpy.context.selected_sequences[0];
 
-    def create_transform(self, frame_start, frame_end):
+    def create_transform_strip(self, frame_start, frame_end):
         bpy.ops.sequencer.effect_strip_add(frame_start=frame_start, frame_end=frame_end, type='TRANSFORM')
+        return bpy.context.selected_sequences[0];
 
     def create_cross(self, first_strip, second_strip):
         bpy.ops.sequencer.select_all(action="DESELECT")
         first_strip.select = True
         second_strip.select = True
-        # The order in which strips are selected determines which direction the cross effect will be applied.
-        # There was no way to change the order the strips are stored in bpy.context.selected_sequences (read only?).
-        # Instead if their order is incorrect the direction of added effect is controlled by use_reverse_frames property.
-        use_reverse_frames = bpy.context.selected_sequences[0].frame_start > bpy.context.selected_sequences[
-            1].frame_start;
+
         bpy.ops.sequencer.effect_strip_add(
             frame_start=second_strip.frame_start,
             frame_end=second_strip.frame_start + self.strips_cross_frames,
             type='CROSS'
         )
-        effect_strip = bpy.context.selected_sequences[0];
-        effect_strip.use_reverse_frames = use_reverse_frames
 
 # if __name__ == "__main__":
 #    bpy.utils.register_class(ImportFiles)
--- operator_ken_burns.py
+++ operator_ken_burns.py
@@ -19,7 +19,15 @@ class KenBurnsEffect(bpy.types.Operator, preferences.KenBurnsEffectPreferences):
         """ Generate Ken Burns effect for selected transform strip """
         transform_strip = bpy.context.selected_sequences[0]
         transform_strip.use_uniform_scale = True
-        
+
+        seq = bpy.context.selected_sequences[0]
+        seq.scale_start_x = 1.0
+        seq.keyframe_insert(data_path='scale_start_x', frame=seq.frame_start)
+        # last added fcurve's keyframe_points interpolation
+        bpy.context.scene.animation_data.action.fcurves[-1].keyframe_points[0].interpolation = "LINEAR"
+        seq.scale_start_x = 1.3
+        seq_last_frame = seq.frame_start + seq.frame_duration - 1;
+        seq.keyframe_insert(data_path='scale_start_x', frame=seq_last_frame)
 
 # if __name__ == "__main__":
 #    bpy.utils.register_class(ImportFiles)
