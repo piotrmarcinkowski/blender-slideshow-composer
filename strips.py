bbe1cdd47408b408b58ed5357a3b625749f845aa
Piotr Marcinkowski <piotrmarc@gmail.com>
2018-03-04 19:54:08 +0100

    Add movie files importing

--- strips.py
+++ strips.py
@@ -3,6 +3,7 @@ import os.path
 from . import preferences
 from . import file_utils
 
+
 class StripsCreator(preferences.StripsCreatorPreferences):
     def create_strips(self, files):
         """ Creates strips for given files """
@@ -12,56 +13,53 @@ class StripsCreator(preferences.StripsCreatorPreferences):
 
         print("Total files to import: {}".format(len(files)))
         for file in files:
-            #temporary
-            print("Creating strip for: {}".format(file))
-            if not file_utils.is_image(file):
-                continue
+            (directory, file_name) = os.path.split(file)
+            file_list = [{'name': file_name}]
 
             frame_start = frame_current
-            frame_end = frame_current + self.image_strip_frames
-            current_strip = self.create_strip(
-                path=file,
-                frame_start=frame_start,
-                frame_end=frame_end,
-                channel=1)
+            current_transform_strip = None
 
-            current_transform_strip = self.create_transform_strip(
-                frame_start=frame_start,
-                frame_end=frame_end)
+            if file_utils.is_image(file):
+                # Add image strip
+                frame_end = frame_current + self.image_strip_frames
+                bpy.ops.sequencer.image_strip_add(
+                    directory=directory,
+                    files=file_list,
+                    frame_start=frame_start,
+                    frame_end=frame_end)
+                current_strip = bpy.context.selected_sequences[0]
+                # Add transform strip for image
+                current_transform_strip = self.create_transform_strip(
+                    frame_start=frame_start,
+                    frame_end=frame_end)
+                current_strip.mute = True
+                # Generate KenBurns effect on added transform strip if enabled
+                if self.generate_ken_burns_effect:
+                    bpy.ops.slideshow_composer.ken_burns_effect('EXEC_DEFAULT')
+            elif file_utils.is_movie(file):
+                # Add movie strip
+                bpy.ops.sequencer.movie_strip_add(
+                    filepath=file,
+                    files=file_list,
+                    frame_start=frame_start)
+                current_strip = bpy.context.selected_sequences[0]
+                frame_end = current_strip.frame_final_end
 
-            current_strip.mute = True
-
-            if self.generate_ken_burns_effect:
-                bpy.ops.slideshow_composer.ken_burns_effect('EXEC_DEFAULT')
-
+            # Create cross effect
+            cross_first_strip = previous_strip
+            cross_second_strip = current_strip
             if previous_transform_strip is not None:
+                cross_first_strip = previous_transform_strip
+            if current_transform_strip is not None:
+                cross_second_strip = current_transform_strip
+            if cross_first_strip is not None and cross_second_strip is not None:
                 self.create_cross(
-                    first_strip=previous_transform_strip,
-                    second_strip=current_transform_strip)
+                    first_strip=cross_first_strip,
+                    second_strip=cross_second_strip)
 
             frame_current = frame_end + 1 - self.strips_cross_frames
             previous_strip = current_strip
             previous_transform_strip = current_transform_strip
-
-    def create_strip(self, path, frame_start, frame_end, channel):
-        if file_utils.is_image(path):
-            return self.create_image_strip(path, frame_start, frame_end, channel)
-        else:
-            return None
-
-    def create_image_strip(self, path, frame_start, frame_end, channel):
-
-        (directory, file) = os.path.split(path)
-        file_list = [{'name': file}]
-
-        bpy.ops.sequencer.image_strip_add(
-            directory=directory,
-            files=file_list,
-            show_multiview=False,
-            frame_start=frame_start,
-            frame_end=frame_end,
-            channel=channel)
-
         return bpy.context.selected_sequences[0];
 
     def create_transform_strip(self, frame_start, frame_end):
